42a34bde363cf086d73cc5d6fb8725317d7b788f
AceReX <ace.rexnoir@gmail.com>
2024-04-08 18:29:40 +0530

    General Commit

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ import { createHtmlElement, getInputValuebyId, checkSize ,resizeGrid, msgLog} fr
 
 let totalGrid = 16; // meaning 16 * 16
 let container = document.querySelector(".container");
+createGrid(totalGrid, container);
 
-for (let i = 0; i < totalGrid; i ++){
+function createGrid(totalGrid, container){
+    for (let i = 0; i < totalGrid; i ++){
 
-    let divRow = createHtmlElement('div', 'row');
-
-    for (let j = 0; j < totalGrid; j++){
-        
-        let divColum = createHtmlElement('div', 'item');
-        divRow.appendChild(divColum);
-
+        let divRow = createHtmlElement('div', 'row');
+    
+        for (let j = 0; j < totalGrid; j++){
+            
+            let divColum = createHtmlElement('div', 'item');
+            divRow.appendChild(divColum);
+    
+        }
+    
+        container.appendChild(divRow);
+    
     }
-
-    container.appendChild(divRow);
-
 }
 
 //Resize button handler
 
 let resizeButton = document.querySelector("form");
 resizeButton.addEventListener("submit", resizeGrid);
+
+//Random colors
+let random = false;
